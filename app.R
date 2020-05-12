27d5970beec7d3740c47183c4309cca11f9002d3
Maële <mmaleysson@etu.isara.fr>
2020-05-12 16:55:39 +0200

    Importater une image

--- app.R
+++ app.R
@@ -9,7 +9,7 @@
 library(shiny)
 
 
-# Création de cases à cocher
+## Création de cases à cocher
 
 
 # PARTIE UI
@@ -18,24 +18,45 @@ ui <- fluidPage(
   # On fait des cases à cocher
   radioButtons("Statut", label = h2("Quel statut avez-vous ? "), 
                      choices = list("Etudiant(e)" = 1, "Apprenti(e)" = 2),
-                     selected = 1),
-  
+                     selected = 2),
+  hr(),
+  fluidRow(column(2, verbatimTextOutput("value2"))),
+           
   radioButtons("Statut", label = h2("Quel est votre sexe ? "), 
                choices = list("Femme" = 1, "Homme" = 2),
-               selected = 1),
+               selected = 2),
   
   hr(),
-  fluidRow(column(2, verbatimTextOutput("value")))
+  fluidRow(column(2, verbatimTextOutput("value"))),
   
-  
+  titlePanel("Importation"),
+
+sidebarLayout(
+  sidebarPanel(
+    fileInput(
+      inputId = "image1",
+      label = "Importer une image",
+      accept = c('image/png', 'image/jpeg','image/jpg')
+    ),
+    tags$hr()
+  ),
+  mainPanel(
+    textOutput("filename"),
+    imageOutput(outputId = "Image")
+  )
+)
 )
 
 # PARTIE SERVER
 server <- function(input, output) {
   
-  # On crée l'output
+  # On affiche l'output
   output$value <- renderPrint({ input$radio })
+  output$value2 <- renderPrint({ input$radio })
   
+  re1 <- reactive({gsub("\\\\", "/", input$image1$datapath)})
+  
+  output$Image <- renderImage({list(src = re1())})
 }
 
 shinyApp(ui, server)
